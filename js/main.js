dfc36fae6b9bd38f086e444ac74c242390167421
Collo <mutaic84@gmail.com>
2022-03-14 22:56:58 +0300

    use a prototype to create a contact's full name and display the full name within the list

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ function Contact(first, last) {
     this.lastName = last;
 }
 
+Contact.prototype.fullName = function () {
+    return this.firstName + " " + this.lastName;
+}
+
 $(document).ready(function () {
     $("form#new-contact").submit(function (e) { 
         e.preventDefault();
@@ -12,7 +16,14 @@ $(document).ready(function () {
 
         let newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+        })
 
         $("input#new-first-name").val("")
         $("input#new-last-name").val("")
